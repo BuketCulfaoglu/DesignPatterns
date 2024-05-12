b5e82cdc68b3d691c54b634d6615370b73a9ef40
anti-pc <buketculfaoglu@gmail.com>
2024-05-12 20:09:54 +0300

    additional builder class added to builderpattern

---
+++ CreationalPatterns/BuilderPattern/PepperoniPizzaBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPattern
+{
+    internal class PepperoniPizzaBuilder : IPizzaBuilder
+    {
+        private Pizza _pizza = new Pizza();
+
+        public void BuildDough()
+        {
+            _pizza.Dough = "thick crust";
+        }
+
+        public void BuildSauce()
+        {
+            _pizza.Sauce = "tomato";
+        }
+
+        public void BuildTopping()
+        {
+            _pizza.Topping = "pepperoni slices";
+        }
+
+        public Pizza GetPizza()
+        {
+            return _pizza;
+        }
+    }
+}
--- CreationalPatterns/BuilderPattern/Program.cs
+++ CreationalPatterns/BuilderPattern/Program.cs
@@ -1,10 +1,17 @@
 ﻿using BuilderPattern;
 
 var margheritaBuilder = new MargheritaPizzaBuilder();
+var pepperoniBuilder = new PepperoniPizzaBuilder();
 
-var director = new PizzaDirector(margheritaBuilder);
 
-director.ConstructPizza();
+var margheritaDirector = new PizzaDirector(margheritaBuilder);
+var pepperoniDirector = new PizzaDirector(pepperoniBuilder);
+
+margheritaDirector.ConstructPizza();
+pepperoniDirector.ConstructPizza();
+
 var margheritaPizza = margheritaBuilder.GetPizza();
+var pepperoniPizza = pepperoniBuilder.GetPizza();
 
 margheritaPizza.Display();
+pepperoniPizza.Display();
