39cc69d873583229ec9c6f0d620d1558238bd378
buketculfaoglu <buketculfaoglu@gmail.com>
2024-06-28 19:05:31 +0300

    Command Pattern added
    By using the Command pattern, we decouple the sender (RemoteControl) from the receiver (Light), allowing us to add new commands without changing existing code. This pattern adheres to the Open/Closed Principle and makes the system more flexible and extensible.

---
+++ BehavioralPatterns/CommandPattern/Light.cs
@@ -0,0 +1,14 @@
+﻿namespace CommandPattern;
+
+public class Light 
+{
+    public void On()
+    {
+        Console.WriteLine("The light is on.");
+    }
+
+    public void Off()
+    {
+        Console.WriteLine("The light is off.");
+    }
+}
---
+++ BehavioralPatterns/CommandPattern/LightOnCommand.cs
@@ -0,0 +1,21 @@
+﻿namespace CommandPattern;
+
+public class LightOnCommand : ICommand
+{
+    private readonly Light _light;
+
+    public LightOnCommand(Light light)
+    {
+        _light = light;
+    }
+
+    public void Execute()
+    {
+        _light.On();    
+    }
+
+    public void Undo()
+    {
+        _light.Off();
+    }
+}
