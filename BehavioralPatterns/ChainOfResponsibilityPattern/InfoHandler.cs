c50eeb33cae58ece4466a6f10cdb18db2165f1a0
buketculfaoglu <buketculfaoglu@gmail.com>
2024-06-30 13:19:06 +0300

    Chain of Responsibility Pattern implementation

---
+++ BehavioralPatterns/ChainOfResponsibilityPattern/InfoHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace ChainOfResponsibilityPattern;
+
+public class InfoHandler : Handler
+{
+    public override void HandleRequest(LogMessage request)
+    {
+        if (request.Level == LogLevel.Info)
+            Console.WriteLine($"Info: {request.Message}");
+        else if (_successor != null)
+            _successor.HandleRequest(request);
+    }
+}
---
+++ BehavioralPatterns/ChainOfResponsibilityPattern/LogMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace ChainOfResponsibilityPattern;
+
+public class LogMessage
+{
+    public string Message { get; }
+    public LogLevel Level { get; }
+
+    public LogMessage(string message, LogLevel level)
+    {
+        Message = message;
+        Level = level;
+    }
+
+}
