1b0589bc67519646255b6860844054ae6a6785e3
buketculfaoglu <buketculfaoglu@gmail.com>
2024-07-06 22:09:53 +0300

    InterpreterPattern example included

---
+++ BehavioralPatterns/InterpreterPattern/VariableExpression.cs
@@ -0,0 +1,14 @@
+﻿public class VariableExpression : AbstractExpression
+{
+    private readonly string _name;
+
+    public VariableExpression(string name)
+    {
+        _name = name;
+    }
+
+    public override int Interpret(Context context)
+    {
+        return context.GetVariable(_name);
+    }
+}
